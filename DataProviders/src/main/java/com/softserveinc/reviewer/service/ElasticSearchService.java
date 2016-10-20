eab6d711096e4156c6261fd414fb56f9707f83b9
Fidel <mykytchyn1985@gmail.com>
2016-10-20 15:09:32 +0300

    elastic search service, added blank reviews

--- DataProviders/src/main/java/com/softserveinc/reviewer/service/ElasticSearchService.java
+++ DataProviders/src/main/java/com/softserveinc/reviewer/service/ElasticSearchService.java
@@ -2,11 +2,27 @@ package com.softserveinc.reviewer.service;
 
 import com.softserveinc.reviewer.model.Review;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ElasticSearchService {
+
+/*    private static final List<List<Review>> REVIEWS_COLLECTION = Arrays.asList(
+            new Review[5], new ArrayList<Review>(12), new ArrayList<Review>(3),
+            new ArrayList<Review>(76), new ArrayList<Review>(321), new ArrayList<Review>(175), new ArrayList<Review>(221),
+            new ArrayList<Review>(51), new ArrayList<Review>(23), new ArrayList<Review>(17), new ArrayList<Review>(93),
+            new ArrayList<Review>(341), new ArrayList<Review>(321),
+            new ArrayList<Review>(56), new ArrayList<Review>(23), new ArrayList<Review>(72), new ArrayList<Review>(35)
+    );*/
+
+    private static final List<Review[]> REVIEWS_LIST = Arrays.asList(
+            new Review[5], new Review[12], new Review[3],
+            new Review[76], new Review[321], new Review[175], new Review[221],
+            new Review[51], new Review[23], new Review[17], new Review[93],
+            new Review[341], new Review[321],
+            new Review[56], new Review[23], new Review[72], new Review[35]
+    );
 
     private static final List<Review> REVIEWS = Arrays.asList(
             new Review("oak-style", "oak-sc-2ft", "2016-07-18T17:00:43.000Z",
@@ -24,9 +40,34 @@ public class ElasticSearchService {
                     "Pellentesque habitant morbi tristique", "review")
     );
 
+    static {
+        for (Review[] reviews : REVIEWS_LIST) {
+            for(int i = 0; i < reviews.length; i++) {
+                reviews[i] = new Review();
+            }
+        }
+        REVIEWS_LIST.get(0)[0] = REVIEWS.get(0);
+        REVIEWS_LIST.get(3)[0] = REVIEWS.get(1);
+    }
+
+/*    public List<Review> getReviews(String type, String client, String externalId) {
+
+        return REVIEWS.stream().filter(x ->  x.getExternalId().equals(externalId) && x.getClient().equals(client) &&
+                x.getType().equals(type)).collect(Collectors.toList());
+    }*/
+
     public List<Review> getReviews(String type, String client, String externalId) {
 
-        return REVIEWS.stream().filter(x -> x.getType().equals(type) && x.getClient().equals(client) &&
-                x.getExternalId().equals(externalId)).collect(Collectors.toList());
+        for (Review[] reviews : REVIEWS_LIST) {
+            if(reviews != null && validateReviewFieldsNotNull(reviews[0]) && reviews[0].getExternalId().equals(externalId) &&
+                    reviews[0].getClient().equals(client) && reviews[0].getType().equals(type)) {
+                return Arrays.asList(reviews);
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    private boolean validateReviewFieldsNotNull(Review review) {
+        return review.getExternalId() != null && review.getClient() != null && review.getType() != null;
     }
 }
