01436623adefaef2a63c9fce0ce09d1860ecc549
Fidel <mykytchyn1985@gmail.com>
2016-10-26 11:29:03 +0300

    reading properties from config using annotations

---
+++ Reviewer/src/main/java/com/softserveinc/reviewer/injector/GuiceModule.java
@@ -0,0 +1,32 @@
+package com.softserveinc.reviewer.injector;
+
+import com.google.inject.Inject;
+import com.google.inject.AbstractModule;
+import com.softserveinc.reviewer.ReviewerConfiguration;
+import com.softserveinc.reviewer.annotation.ElasticSearchBaseUrl;
+import com.softserveinc.reviewer.annotation.ElasticSearchUri;
+import com.softserveinc.reviewer.annotation.OracleBaseUrl;
+import com.softserveinc.reviewer.annotation.OracleUri;
+import com.softserveinc.reviewer.annotation.SyndicationBaseUrl;
+import com.softserveinc.reviewer.annotation.SyndicationUri;
+
+public class GuiceModule extends AbstractModule {
+
+    private final ReviewerConfiguration configuration;
+
+    @Inject
+    public GuiceModule(ReviewerConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public void configure() {
+        bindConstant().annotatedWith(ElasticSearchBaseUrl.class).to(configuration.getElasticSearchBaseUrl());
+        bindConstant().annotatedWith(ElasticSearchUri.class).to(configuration.getElasticSearchUri());
+        bindConstant().annotatedWith(OracleBaseUrl.class).to(configuration.getOracleBaseUrl());
+        bindConstant().annotatedWith(OracleUri.class).to(configuration.getOracleUri());
+        bindConstant().annotatedWith(SyndicationBaseUrl.class).to(configuration.getSyndicationBaseUrl());
+        bindConstant().annotatedWith(SyndicationUri.class).to(configuration.getSyndicationUri());
+
+    }
+}
