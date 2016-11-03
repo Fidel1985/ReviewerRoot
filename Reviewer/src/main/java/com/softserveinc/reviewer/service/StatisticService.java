e6b1757f1df6a71571983494a9ec6e8794db3120
Fidel <mykytchyn1985@gmail.com>
2016-11-03 12:20:19 +0200

    update collection

--- Reviewer/src/main/java/com/softserveinc/reviewer/service/StatisticService.java
+++ Reviewer/src/main/java/com/softserveinc/reviewer/service/StatisticService.java
@@ -1,15 +1,20 @@
 package com.softserveinc.reviewer.service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.softserveinc.reviewer.model.Statistic;
 import org.mongojack.DBCursor;
 import org.mongojack.DBQuery;
 import org.mongojack.JacksonDBCollection;
-import org.mongojack.WriteResult;
 
 public class StatisticService {
     private Provider<MongoClient> mongoClientProvider;
@@ -21,23 +26,32 @@ public class StatisticService {
 
     public void saveStats(double duration, String methodName) {
         JacksonDBCollection<Statistic, String> collection = JacksonDBCollection.wrap(getCollection(), Statistic.class, String.class);
-        DBCursor<Statistic> cursor = collection.find(DBQuery.is("methodName", "getSyndicationMatches"));
-        Statistic statistic = cursor.next();
-        if(statistic != null) {
-            int invocationCount = statistic.getInvocationCount();
-            double oldAverage = statistic.getAverage();
-            double newAverage = (invocationCount * oldAverage + duration) / ++invocationCount;
-            statistic.setInvocationCount(invocationCount);
-            statistic.setAverage(newAverage);
-
-            collection.findAndModify(DBQuery.is("methodName", "getSyndicationMatches"), statistic, null, true, null, true, true);
-        }
-        else {
+        DBObject query = new BasicDBObject("name", "getSyndicationMatches");
+        DBCursor<Statistic> cursor = collection.find(DBQuery.is("name", "getSyndicationMatches"));
+        Statistic statistic;
+        try {
+            statistic = cursor.next();
+            if (statistic != null) {
+                int invocationCount = statistic.getInvocationCount();
+                double oldAverage = statistic.getAverage();
+                double newAverage = (invocationCount * oldAverage + duration) / ++invocationCount;
+                DBObject update = new BasicDBObject();
+                Map<String, Object> map = new HashMap<>();
+                map.put("average", Math.round(newAverage * 10.0) / 10.0);
+                map.put("count", invocationCount);
+                //update.put("$set", new BasicDBObject("average", Math.round(newAverage * 10.0) / 10.0));
+                //update.put("$set", new BasicDBObject("count", invocationCount));
+                //statistic.setAverage(Math.round(newAverage * 10.0) / 10.0);
+                //statistic.setInvocationCount(invocationCount);
+                update.put("$set", map);
+                collection.findAndModify(query, update);
+            }
+        } catch (NoSuchElementException e) {
             statistic = new Statistic();
             statistic.setMethodName(methodName);
             statistic.setInvocationCount(1);
             statistic.setAverage(Math.round(duration * 10.0) / 10.0);
-            WriteResult<Statistic, String> result = collection.insert(statistic);
+            collection.insert(statistic);
         }
     }
 
