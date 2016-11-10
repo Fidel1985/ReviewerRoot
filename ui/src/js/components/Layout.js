08cccffbb07412aeee3c75203e55f84add4f43d7
Fidel <mykytchyn1985@gmail.com>
2016-11-10 16:10:31 +0200

    react change DOM structure where it is needed only

---
+++ ui/src/js/components/Layout.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = {name : "Andrew"};
+  }
+
+  render() {
+    setTimeout(() => {
+      this.setState({name: "Bob"});
+    }, 1000);
+    return (
+      <div>
+        {this.state.name}
+        <Header/>
+        <Footer/>
+      </div>
+    );
+  }
+}
