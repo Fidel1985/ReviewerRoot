04e637841edeedd319eb62dea4d5c9d0bd1a2f99
Andrii Mykytchyn <andrii.mykytchyn@bazaarvoice.com>
2016-12-19 17:07:57 +0200

    convert Layout to functioanl component, fix small bugs

--- ReviewerUI/src/js/components/Layout.js
+++ ReviewerUI/src/js/components/Layout.js
@@ -3,13 +3,11 @@ import React from "react/lib/React";
 import Header from "./Header";
 import SearchCriteriaForm from "./SearchCriteriaForm";
 
-export default class Layout extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <SearchCriteriaForm />
-      </div>
-    );
-  }
+export default function Layout() {
+  return (
+    <div>
+      <Header />
+      <SearchCriteriaForm />
+    </div>
+  );
 }
